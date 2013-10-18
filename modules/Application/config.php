399a0a2f06937228ec0a525275cec3b923aafc66
justinbcain <justin@unknown7cc3a171d9df.gateway.pace.com>
2013-10-18 16:44:47 -0500

    Added Module Config
    Allowed module specific configuration of Header and Helpers.

---
+++ modules/Application/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+	'header'=>array(
+		'headScripts' => array(),
+		'headLinks' => array(),
+		'footerScripts' => array(),
+		'footerInlineScripts' => array(),
+		'headInlineScripts' =>array()
+		),
+
+	'viewHelpers' => array(),
+	'controllerHelpers' => array()
+)
